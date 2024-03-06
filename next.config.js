051337dd54a8c79a875832c954770327109ca38e
hyunu0831 <hyunu.dev@gmail.com>
2024-03-06 16:24:20 +0900

    feat: mobx-react 관련 설정 추가

--- next.config.js
+++ next.config.js
@@ -4,4 +4,12 @@ module.exports = {
     // ssr, displayName true가 기본값으로 켜진다.
     styledComponents: true,
   },
+  webpack: (config, { isServer }) => {
+    // 필요한 webpack 설정 추가
+    if (!isServer) {
+      config.resolve.alias['mobx-react'] = 'mobx-react-lite';
+    }
+
+    return config;
+  },
 };
